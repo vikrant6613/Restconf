eea1ea54b8b33921eb57a1d72a4d5a0c7db23ab9
Vikrant Kumar <vikrant.hacks@gmail.com>
2022-02-24 19:47:56 +0530

    programs updated

--- programs/06_adding _interfaces.py
+++ programs/06_adding _interfaces.py
@@ -1,8 +1,8 @@
 import requests
 import json
 
-device = json.loads(open("device_login.json").read())
-module = 'ietf-interfaces:interfaces/interface'
+device = json.loads(open("local-device_login.json").read())
+module = 'Cisco-IOS-XE-native:native/interface'
 url = f"https://{device['host']}/restconf/data/"
 data_filter ={ 
     "ietf-interfaces:interface": {
@@ -25,17 +25,18 @@ data_filter ={
 int_name = data_filter["ietf-interfaces:interface"]['name']
 requests.packages.urllib3.disable_warnings()
 
-for int_number in range(101,105):
-    data_filter["ietf-interfaces:interface"]["name"] = f'{int_name}{int_number}'
-    data_filter["ietf-interfaces:interface"]["description"] = f'{data_filter["ietf-interfaces:interface"]["name"]} configured by RESTCONF-ERAGON'
-    data_filter["ietf-interfaces:interface"]["ietf-ip:ipv4"]["address"][0]["ip"] = f'10.20.30.{int_number}'
-    payload = json.dumps(data_filter)
-    print(payload)
+# for int_number in range(101,105):
+#     data_filter["ietf-interfaces:interface"]["name"] = f'{int_name}{int_number}'
+#     data_filter["ietf-interfaces:interface"]["description"] = f'{data_filter["ietf-interfaces:interface"]["name"]} configured by RESTCONF-ERAGON'
+#     data_filter["ietf-interfaces:interface"]["ietf-ip:ipv4"]["address"][0]["ip"] = f'10.20.30.{int_number}'
+#     payload = json.dumps(data_filter)
+#     print(payload)
     
-    response_post = requests.patch(f"{url}{module}", headers=device['headers'], auth=(device['username'], device['password']), verify=False, data=payload)
-    print(response_post.text)
+#     response_post = requests.patch(f"{url}{module}", headers=device['headers'], auth=(device['username'], device['password']), verify=False, data=payload)
+#     print(response_post.status_code)
+#     response_post.close
 
 response_get = requests.get(f"{url}{module}", headers=device['headers'], auth=(device['username'], device['password']), verify=False).json()
 
 # print(response_post.text)
-print(response_get)
+print(json.dumps(response_get))
