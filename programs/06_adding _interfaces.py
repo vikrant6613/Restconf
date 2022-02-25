a2a02444e46b4c960c8baf6bf24656aad8d4190d
Vikrant Kumar <vikrant.hacks@gmail.com>
2022-02-25 21:48:09 +0530

    programs updated

--- programs/06_adding _interfaces.py
+++ programs/06_adding _interfaces.py
@@ -1,8 +1,8 @@
 import requests
 import json
 
-device = json.loads(open("local-device_login.json").read())
-module = 'Cisco-IOS-XE-native:native/interface'
+device = json.loads(open("device_login.json").read())
+module = 'ietf-interfaces:interfaces/interface'
 url = f"https://{device['host']}/restconf/data/"
 data_filter ={ 
     "ietf-interfaces:interface": {
@@ -14,7 +14,7 @@ data_filter ={
           "address": [
             {
               "ip": "",
-              "netmask": "255.255.255.0"
+              "netmask": "255.255.255.255"
             }
           ]
         },
@@ -25,18 +25,15 @@ data_filter ={
 int_name = data_filter["ietf-interfaces:interface"]['name']
 requests.packages.urllib3.disable_warnings()
 
-# for int_number in range(101,105):
-#     data_filter["ietf-interfaces:interface"]["name"] = f'{int_name}{int_number}'
-#     data_filter["ietf-interfaces:interface"]["description"] = f'{data_filter["ietf-interfaces:interface"]["name"]} configured by RESTCONF-ERAGON'
-#     data_filter["ietf-interfaces:interface"]["ietf-ip:ipv4"]["address"][0]["ip"] = f'10.20.30.{int_number}'
-#     payload = json.dumps(data_filter)
-#     print(payload)
-    
-#     response_post = requests.patch(f"{url}{module}", headers=device['headers'], auth=(device['username'], device['password']), verify=False, data=payload)
-#     print(response_post.status_code)
-#     response_post.close
+for int_number in range(101,105):
+    data_filter["ietf-interfaces:interface"]["name"] = f'{int_name}{int_number}'
+    data_filter["ietf-interfaces:interface"]["description"] = f'{data_filter["ietf-interfaces:interface"]["name"]} configured by RESTCONF-ERAGON'
+    data_filter["ietf-interfaces:interface"]["ietf-ip:ipv4"]["address"][0]["ip"] = f'10.20.30.{int_number}'
+    payload = json.dumps(data_filter)
+    # print(payload)    
+    response_patch = requests.patch(f"{url}{module}", headers=device['headers'], auth=(device['username'], device['password']), verify=False, data=payload)
 
-response_get = requests.get(f"{url}{module}", headers=device['headers'], auth=(device['username'], device['password']), verify=False).json()
-
-# print(response_post.text)
-print(json.dumps(response_get))
+    if response_patch.status_code == 204:
+      print(f'Interface {data_filter["ietf-interfaces:interface"]["name"]} created successfully ')
+    else:
+      print(f'respone code : {response_patch.status_code}. Interface {data_filter["ietf-interfaces:interface"]["name"]} not created')
